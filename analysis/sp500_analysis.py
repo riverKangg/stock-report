f684f161d225389062380930a8df42e57e2a52ab
riverkangg <riverkangg@gmail.com>
2023-10-15 11:18:59 +0900

    Modify variable names

--- analysis/sp500_analysis.py
+++ analysis/sp500_analysis.py
@@ -7,7 +7,7 @@ from stock_report.sp500_list_fetcher import SP500ListFetcher
 
 warnings.filterwarnings('ignore')
 
-def analyze_or_load_sp500_data(save_directory):
+def fetch_or_load_sp500_data(save_directory):
     data_file_path = os.path.join(save_directory, 'sp500_valuation_data.csv')
 
     # Check if the data file already exists
@@ -17,10 +17,11 @@ def analyze_or_load_sp500_data(save_directory):
         return valuation_data
 
     # Step 1: Get S&P 500 tickers
+    print('Start Fetching')
     sp500_data = SP500ListFetcher().get_sp500_data()
     sp500_tickers = sp500_data.index.tolist()
 
-    # Step 2: Analyze S&P 500 stocks
+    # Step 2: Fetch S&P 500 stocks
     analyzed_data = {}
     error_tickers = []  # 에러가 발생한 티커 목록
 
@@ -51,14 +52,14 @@ def add_sp500_info(valuation_data):
     valuation_data = valuation_data.join(sp500_data)
     return valuation_data
 
-def save_top_securities_to_csv(top_securities, metric, save_directory, group_by=None):
+def save_top_stocks_to_csv(top_stocks, metric, save_directory, group_by=None):
     if group_by:
         csv_file_name = f'top_{metric}_{group_by}.csv'
     else:
         csv_file_name = f'top_{metric}.csv'
 
     csv_file_path = os.path.join(save_directory, csv_file_name)
-    top_securities.to_csv(csv_file_path, sep="\t", index=False)
+    top_stocks.to_csv(csv_file_path, sep="\t", index=False)
 
 
 def save_grouped_data_to_csv(grouped_data, metric, save_directory):
@@ -67,7 +68,7 @@ def save_grouped_data_to_csv(grouped_data, metric, save_directory):
     grouped_data.to_csv(csv_file_path, sep="\t")
 
 
-def get_top_n_securities(valuation_data, metric, n=10, ascending=True, group_by=None):
+def get_top_n_stocks(valuation_data, metric, n=10, ascending=True, group_by=None):
     if metric not in valuation_data.columns:
         print(f"Column '{metric}' not found in the data.")
         return None
@@ -75,10 +76,10 @@ def get_top_n_securities(valuation_data, metric, n=10, ascending=True, group_by=
     if group_by:
         valuation_data = add_sp500_info(valuation_data)
         valuation_data = valuation_data[valuation_data[metric]>0]
-        top_securities = valuation_data.groupby(group_by).apply(lambda x: x.nsmallest(n, metric))[[metric,'MarketCap']].reset_index()
-        top_securities.columns = [group_by, 'Symbol', metric, 'MarketCap']
-        top_securities[metric] = top_securities[metric].apply(lambda x: round(x, 1))
-        top_securities['MarketCap'] = top_securities['MarketCap'].apply(lambda x: numerize.numerize(x))
+        top_stocks = valuation_data.groupby(group_by).apply(lambda x: x.nsmallest(n, metric))[[metric,'MarketCap']].reset_index()
+        top_stocks.columns = [group_by, 'Symbol', metric, 'MarketCap']
+        top_stocks[metric] = top_stocks[metric].apply(lambda x: round(x, 1))
+        top_stocks['MarketCap'] = top_stocks['MarketCap'].apply(lambda x: numerize.numerize(x))
 
         # Calculating the Number of Companies and Average Metric by Sector.
         grouped_data = valuation_data.groupby(group_by)[metric].agg(['count', 'mean'])
@@ -87,30 +88,30 @@ def get_top_n_securities(valuation_data, metric, n=10, ascending=True, group_by=
         save_grouped_data_to_csv(grouped_data, metric, save_directory)
 
     else:
-        top_securities = valuation_data.nlargest(n, metric)[[metric]]
-        top_securities[metric] = top_securities[metric].apply(lambda x: numerize.numerize(x))
-        top_securities = top_securities.reset_index()
-        top_securities.columns = ['Symbol', metric]
+        top_stocks = valuation_data.nlargest(n, metric)[[metric]]
+        top_stocks[metric] = top_stocks[metric].apply(lambda x: numerize.numerize(x))
+        top_stocks = top_stocks.reset_index()
+        top_stocks.columns = ['Symbol', metric]
 
-    return top_securities
+    return top_stocks
 
 
 if __name__ == "__main__":
     save_directory = "sp500_data"
 
-    analyzed_data = analyze_or_load_sp500_data(save_directory)
+    analyzed_data = fetch_or_load_sp500_data(save_directory)
 
     if analyzed_data is not None:
         print(analyzed_data.shape)
 
         # Get the top 10 MarketCap stocks and save to CSV:
-        top_10_market_cap_stocks = get_top_n_securities(analyzed_data, 'MarketCap', n=10, ascending=False)
-        save_top_securities_to_csv(top_10_market_cap_stocks, 'MarketCap', save_directory)
+        top_10_market_cap_stocks = get_top_n_stocks(analyzed_data, 'MarketCap', n=10, ascending=False)
+        save_top_stocks_to_csv(top_10_market_cap_stocks, 'MarketCap', save_directory)
 
         # Get the top 10 PER Sector and save to CSV:
-        top_10_per_sector = get_top_n_securities(analyzed_data, 'PER', n=5, ascending=True, group_by='Sector')
-        save_top_securities_to_csv(top_10_per_sector, 'PER', save_directory, 'Sector')
+        top_10_per_sector = get_top_n_stocks(analyzed_data, 'PER', n=5, ascending=True, group_by='Sector')
+        save_top_stocks_to_csv(top_10_per_sector, 'PER', save_directory, 'Sector')
 
         # Get the top 10 PBR Sector and save to CSV:
-        top_10_pbr_sector = get_top_n_securities(analyzed_data, 'PBR', n=5, ascending=True, group_by='Sector')
-        save_top_securities_to_csv(top_10_pbr_sector, 'PBR', save_directory, 'Sector')
+        top_10_pbr_sector = get_top_n_stocks(analyzed_data, 'PBR', n=5, ascending=True, group_by='Sector')
+        save_top_stocks_to_csv(top_10_pbr_sector, 'PBR', save_directory, 'Sector')
