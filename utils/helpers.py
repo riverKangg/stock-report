3e6a3ddb3b1e7f5ca64324ae7aaea0203ab86000
riverKangg <riverKangg@gmail.com>
2023-12-01 11:53:53 +0900

    feat: Integrate utility functions

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,11 @@
 import subprocess
+from datetime import datetime
 
-
-def get_git_root_directory():
+def get_root_directory():
     result = subprocess.run(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE, text=True)
-    return result.stdout.strip()
+    root_dir = result.stdout.strip()
+    return root_dir
+
+def get_formmated_date():
+    formatted_date = datetime.now().strftime('%Y-%m-%d')
+    return formatted_date
